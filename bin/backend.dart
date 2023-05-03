7af91c342934df52a27f397eec22e7ea1df60140
lteruya <quemossabe@gmail.com>
2023-05-02 23:32:15 -0300

    backend

---
+++ bin/backend.dart
@@ -0,0 +1,12 @@
+import 'package:shelf/shelf.dart';
+
+import 'api/blog_api.dart';
+import 'api/login_api.dart';
+import 'infra/custom_server.dart';
+
+Future<void> main() async {
+  var cascadeHandler =
+      Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
+
+  await CustomServer().initialize(cascadeHandler);
+}
